b6db42f68b4d04b2c635c0a9cc0e00bb9c71d3d8
James West <james.west@westdarley.co.uk>
2017-10-13 00:28:51 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ScreenOrientation
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ScreenOrientation } from '@ionic-native/screen-orientation';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    private screenOrientation: ScreenOrientation) {
 
   }
+
+  orientation = 'not set';
+
+  ngOnInit() {
+    
+    this.orientation=this.screenOrientation.type;
+
+    this.screenOrientation.onChange().subscribe(val => {
+      const so = this.screenOrientation;
+      this.orientation =  so.type;
+    });
+  }
+
 
 }
